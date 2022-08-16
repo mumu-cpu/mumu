f915c482ba6acac1fe5a198287c8a2f57d310a30
mumu-cpu <62449387+mumu-cpu@users.noreply.github.com>
2022-08-16 19:17:23 +0200

    attachinterrupt

--- tic_3_dia_b_r_s1/tmo.cpp
+++ tic_3_dia_b_r_s1/tmo.cpp
@@ -4,4 +4,28 @@ void manu()
    {
 
    }
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), onEvent, CHANGE);
+  Serial.println(F("Système intialisé"));
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void onEvent() {
+  state = !state;
+  Serial.print(F("Switch LED 13 : "));
+  if(state){
+    Serial.println(F("ON"));
+  }else{
+    Serial.println(F("OFF"));
+  }
 }
