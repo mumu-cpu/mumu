708184cb0436e120c5cb4f79414281ce8467333d
mumu-cpu <62449387+mumu-cpu@users.noreply.github.com>
2022-08-18 10:45:34 +0200

    tic3.1maj

--- TIC_3.1/tmo.cpp
+++ TIC_3.1/tmo.cpp
@@ -12,7 +12,47 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ledPin, state);
+
+int boutonPin  = 2 ;
+int ledPin = 3 ;
+int compteur = 0 ;
+
+//  Pour qu'on puisse changer la valeur d'une variable depuis une fonction de
+//  gestion d'interruption, il faut que la variable soit déclaré "volatile"
+
+volatile boolean changed = false;
+
+
+
+void setup() { 
+  Serial.begin (9600);
+  pinMode(boutonPin, INPUT);
+  pinMode(ledPin,OUTPUT);
+  // Attacher notre fonction ISR, detection de front descendant (5v vers 0v)
+  attachInterrupt(0, doContact, FALLING);
+} 
+
+void loop() {
+  if ( changed ) {
+    changed = false;
+    Serial.print("Ha ! Action sur le BP ");
+    Serial.print(compteur);
+    Serial.println(" fois");
+    // on allume la LED pendant 1 seconde
+    digitalWrite(ledPin,HIGH);
+    delay(1000);
+    digitalWrite(ledPin,LOW);
+    }
+  delay(5000);
+}
+
+
+void doContact() {
+  changed = true;
+  compteur++;
+}
+
+   digitalWrite(ledPin, state);
 }
 
 void onEvent() {
@@ -24,3 +64,6 @@ void onEvent() {
     Serial.println(F("OFF"));
   }
 }
+
+
+
