8c04c88b63a1ff6e311392a19b1710c93e2fdd8f
mumu-cpu <62449387+mumu-cpu@users.noreply.github.com>
2022-08-24 00:34:01 +0200

    nex

---
+++ tic_3_dia_b_r_s1/veilleFonc - demare (fin tempo).cpp
@@ -0,0 +1,7 @@
+veilleFonc - demare (fin tempo)
+veilleF    - alimOffFonc + battOffFonc
+f_b_cl.    - veilleF + alimOffFonc + battOffFonc
+controlAlimFonc - veilleF + demare
+f_r_cl.    - veilleF + veilleFonc + alimOffFonc + battOffFonc
+spot_cl.   - veilleF * veilleFonc + alimOffFonc + battOffFonc + sosNbr10
+
